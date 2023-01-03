90edec24cb0bdef8634ea3cfe28dc52d4a056cb9
TH3-S4LM0N <blakeblankenship@ccawarriors.com>
2023-01-03 15:05:33 -0600

    gnome gone???????????!???!?!?!?!?!?

--- action_prep.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-mkdir -p $SCRIPTPATH/builddir
-cp -r $SCRIPTPATH/silverblue/* $SCRIPTPATH/builddir
-mkdir -p $SCRIPTPATH/builddir/usr/share/leftos
-cp -r $SCRIPTPATH/configs $SCRIPTPATH/builddir/usr/share/leftos
