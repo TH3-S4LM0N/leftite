504e802f5a8294b514030a18069a8bd54591e706
TH3-S4LM0N <blakeblankenship@ccawarriors.com>
2023-01-04 17:21:44 -0600

    Add iso stuff

---
+++ iso/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/evn bash
+
+mkdir -p repo cache
+ostree --repo=repo init --mode=archive
+# unsure whether --unified-core is needed
+sudo rpm-ostree compose tree --repo=repo --cachedir=cache fedora-silverblue.yaml
+ostree summary --repo=repo --update
