bced473009364720be39f1dd04a86114ceded55b
TH3-S4LM0N <blakeblankenship@ccawarriors.com>
2023-01-04 18:58:12 -0600

    logging sorta

--- iso/build.sh
+++ iso/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir -p repo cache
+echo "Creating ostree repo"
 ostree --repo=repo init --mode=archive
 # unsure whether --unified-core is needed
+echo "Composing tree"
 sudo rpm-ostree compose tree --repo=repo --cachedir=cache fedora-silverblue.yaml
+echo "Summarizing"
 ostree summary --repo=repo --update
