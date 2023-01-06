3abc9e2e8cf7adb101c93094bf182af5c15df974
salmon <salmon@City17.lan>
2023-01-05 23:04:06 -0500

    builds iso finally

--- iso/build.sh
+++ iso/build.sh
@@ -8,27 +8,22 @@ ostree --repo=repo init --mode=archive
 echo "Composing tree"
 sudo rpm-ostree compose tree --unified-core --repo=repo --cachedir=cache fedora-silverblue.yaml
 echo "Building ISO with Lorax"
-#lorax \
-#    --product Fedora \
-    #--product Leftite
-#    --version=37 \
-#    --release 20180410 \
-#    --source=https://kojipkgs.fedoraproject.org/compose/37/latest-Fedora-37/compose/Everything/x86_64/os/ \
-#	--variant=Silverblue \
-#    --nomacboot \
-#    --volid=Fedora-Silverblue-ostree-x86_64-37 \
-    # i honestly have no idea whether these 2 are shipped with fedora sb release
-#    --add-template=./lorax-configure-repo.tmpl \
-#    --add-template=./lorax-embed-repo.tmpl \
-lorax  --product=Fedora \
+
+
+
+
+
+
+
+sudo lorax  --product=Fedora \
 		--version=37 \
 		--release=20180410 \
 		--source=https://kojipkgs.fedoraproject.org/compose/37/latest-Fedora-37/compose/Everything/x86_64/os/ \
 		--variant=Silverblue \
 		--nomacboot \
 		--volid=Fedora-SB-ostree-x86_64-37 \
-		--add-template=./lorax-configure-repo.tmpl \
-		--add-template=./lorax-embed-repo.tmpl \
+		--add-template=$(pwd)/lorax-configure-repo.tmpl \
+		--add-template=$(pwd)/lorax-embed-repo.tmpl \
 		--add-template-var=ostree_install_repo=file://$(pwd)/repo \
 		--add-template-var=ostree_update_repo=file://$(pwd)/repo \
 		--add-template-var=ostree_osname=fedora \
@@ -42,3 +37,5 @@ lorax  --product=Fedora \
 		$(pwd)/ostree_installer
 
 sudo setenforce 1
+cp ./ostree_installer/images/boot.iso .
+
