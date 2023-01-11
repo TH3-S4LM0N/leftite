6ccb8f7eb47848857851e14bc68ca49f23c766e4
TH3-S4LM0N <blakeblankenship@ccawarriors.com>
2023-01-10 19:13:00 -0600

    this is actually correct

--- iso/build.sh
+++ iso/build.sh
@@ -6,38 +6,42 @@ if [ "$(whoami)" != "root" ]; then
 	exit 1
 fi
 
+setenforce 0
 mkdir builddir && cd builddir
 
-setenforce 0
-#mkdir -p repo cache
-#ostree --repo=repo init --mode=archive
-#rpm-ostree compose tree --unified-core --repo=repo --cachedir=cache ../fedora-silverblue.yaml
+git clone -b f37 https://pagure.io/workstation-ostree-config.git
+git clone -b f37 https://pagure.io/fedora-lorax-templates.git
+mkdir -p repo cache
+ostree --repo=repo init --mode=archive
+rpm-ostree compose tree \
+	--repo=$(pwd)/repo \
+	--cachedir=$(pwd)/cache \
+	$(pwd)/workstation-ostree-config/fedora-silverblue.yaml
 
-lorax -p Fedora -v 37 -r 37 \
-	-s http://dl.fedoraproject.org/pub/fedora/linux/releases/37/Everything/x86_64/os/ \
-	-s http://dl.fedoraproject.org/pub/fedora/linux/updates/37/x86_64/ \
-	./resulsts/
-
-#lorax  --product=Fedora \
-#		--version=37 \
-#		--release=20180410 \
-#		--source=https://kojipkgs.fedoraproject.org/compose/37/latest-Fedora-37/compose/Everything/x86_64/os/ \
-#		--variant=Silverblue \
-#		--nomacboot \
-#		--volid=Fedora-SB-ostree-x86_64-37 \
-#		--add-template=$(pwd)/../lorax-configure-repo.tmpl \
-#		--add-template=$(pwd)/../lorax-embed-repo.tmpl \
-#		--add-template-var=ostree_install_repo=file://$(pwd)/repo \
-#		--add-template-var=ostree_update_repo=file://$(pwd)/repo \
-#		--add-template-var=ostree_osname=fedora \
-#		--add-template-var=ostree_oskey=fedora-37-primary \
-#		--add-template-var=ostree_contenturl=mirrorlist=https://ostree.fedoraproject.org/mirrorlist \
-#		--add-template-var=ostree_install_ref=fedora/37/x86_64/silverblue \
-#		--add-template-var=ostree_update_ref=fedora/37/x86_64/silverblue \
-#		--logfile=$(pwd)/lorax.log \
-#		--tmp=$(pwd)/tmp \
-#		--rootfs-size=8 \
-#		$(pwd)/ostree_installer
+lorax  --product=Fedora \
+		--version=37 \
+		--release=20180410 \
+		--source=https://kojipkgs.fedoraproject.org/compose/37/latest-Fedora-37/compose/Everything/x86_64/os/ \
+		--variant=Silverblue \
+		--nomacboot \
+		--volid=Fedora-SB-ostree-x86_64-37 \
+		--add-template=$(pwd)/fedora-lorax-templates/ostree-based-installer/lorax-configure-repo.tmpl \
+    	--add-template=$(pwd)/fedora-lorax-templates/ostree-based-installer/lorax-embed-repo.tmpl \
+        --add-template=$(pwd)/fedora-lorax-templates/ostree-based-installer/lorax-embed-flatpaks.tmpl \
+		--add-template-var=ostree_install_repo=file://$(pwd)/repo \
+		--add-template-var=ostree_update_repo=file://$(pwd)/repo \
+		--add-template-var=ostree_osname=fedora \
+		--add-template-var=ostree_oskey=fedora-37-primary \
+		--add-template-var=ostree_contenturl=mirrorlist=https://ostree.fedoraproject.org/mirrorlist \
+		--add-template-var=ostree_install_ref=fedora/37/x86_64/silverblue \
+		--add-template-var=ostree_update_ref=fedora/37/x86_64/silverblue \
+		--add-template-var=flatpak_remote_name=fedora \
+        --add-template-var=flatpak_remote_url=oci+https://registry.fedoraproject.org \
+        --add-template-var=flatpak_remote_refs="runtime/org.fedoraproject.Platform/x86_64/f35 app/org.gnome.gedit/x86_64/stable" \
+		--logfile=$(pwd)/lorax.log \
+		--tmp=$(pwd)/tmp \
+		--rootfs-size=8 \
+		$(pwd)/ostree_installer
 setenforce 1
 cp ./ostree_installer/images/boot.iso ..
 cd ..
